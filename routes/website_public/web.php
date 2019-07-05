82b1c57f38365c8fa34fe43350aabe2ec15a976b
Estarly Olivar <ztrly2022@gmail.com>
2019-07-05 12:43:35 -0400

    Add Modules

---
+++ routes/website_public/web.php
@@ -0,0 +1,33 @@
+<?php
+
+Route::get('', function () {
+    return 'hola';
+});
+
+Route::group([
+//    'middleware' => ['auth'],
+    'namespace' => '\Modules\Blog\Http\Controllers'
+], function () {
+
+    Route::get('/blog', [
+        'as' => 'web.blog',
+        'uses' => 'PageController@blog'
+    ]);
+
+    Route::get('post/{slug}', [
+        'as' => 'web.blog.post',
+        'uses' => 'PageController@post'
+    ]);
+
+    Route::get('category/{slug}', [
+        'as' => 'web.blog.category',
+        'uses' => 'PageController@category'
+    ]);
+
+    Route::get('tag/{slug}', [
+        'as' => 'web.blog.tag',
+        'uses' => 'PageController@tag'
+    ]);
+});
+
+
