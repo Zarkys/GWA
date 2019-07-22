c3d7d0a05d027e87e1296183e612603477d10de6
Estarly Olivar <ztrly2022@gmail.com>
2019-07-22 10:31:17 -0400

    bajando cambios

--- Modules/Records/Http/Controllers/ArchiveController.php
+++ Modules/Records/Http/Controllers/ArchiveController.php
@@ -31,7 +31,8 @@ class ArchiveController extends BaseController
 //    TODO CRUD ARCHIVE
     public function store(Request $request)
     {
-        return $request->all();
+        
+//        return $request->all();
         $mime = ComponentController::Mime($request->file('file')->getMimeType());
 
         $validator = Validator::make($request->all(), [
